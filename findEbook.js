86baa5a40f2cf6b14b99fa6a5f38256d90e3043c
leductienttkt <leductienttkt@gmail.com>
2016-11-18 18:09:17 +0700

    aa

--- findEbook.js
+++ findEbook.js
@@ -1,93 +1,74 @@
-var request = require('request');
-var restify = require('restify');
-var builder = require('botbuilder');
+var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var router = express();
 
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
+var request = require("request");
+
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
 });
 
-// Create bot and bind to Emulator
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+// Ðây là ðo?n code ð? t?o Webhook
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'tiendien35') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
 });
 
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
-var model = 'https://api.projectoxford.ai/luis/v1/application?id=f45e0f47-cf80-4347-b916-d7321d79df59&subscription-key=72eae51afb6f4c2e99587987c6af94b8&q=';
-var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-
-var bookAPI = 'https://www.googleapis.com/books/v1/volumes?q=';
-
-bot.dialog('/', dialog);
-
-// dialog.matches('Greeting', [
-//   function (session, args, next) {
-//     var title = builder.EntityRecognizer.findEntity(args.entities, 'Hello');
-//     var greeting = session.dialogData.greeting = {
-//       title: title ? title.entity : null
-//     };
-//
-//     if (ebook.title) {
-//       session.send('Hello! What ebooks do you want to find?');
-//     }
-//
-//   }
-// ]);
-
-dialog.matches(/^hello/i, [
-  function (session) {
-    session.send('Hello! What ebooks do you want to find?');
-  }
-]);
-
-dialog.matches(/^hi/i, [
-  function (session) {
-    session.send('Hello! What ebooks do you want to find?');
-  }
-]);
-
-// Add intent handlers
-dialog.matches('StartSearching', [
-    function (session, args, next) {
-
-        // Resolve and store any entities passed from LUIS.
-        var title = builder.EntityRecognizer.findEntity(args.entities, 'EbookName');
-        var ebook = session.dialogData.ebook = {
-          title: title ? title.entity : null
-        };
-
-        // Prompt for title
-        if (!ebook.title) {
-            builder.Prompts.text(session, 'What would you like to find ebooks?');
-        } else {
-            next();
+// X? l? khi có ngý?i nh?n tin cho bot
+app.post('/webhook', function(req, res) {
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nh?n ngý?i dùng
+          sendMessage(senderId, "Tui là bot ðây: " + text);
         }
-    },
-    function (session, results, next) {
-      var ebook = session.dialogData.ebook;
-      request({url: bookAPI + ebook.title, json: true}, function(err, res, ebooks) {
-        if (err) {
-          throw err;
-        }
-
-        var index = 0;
-
-        // return 5 ebooks
-        ebooks.items.forEach(function (item) {
-          if (index < 5) {
-            session.send("Ebook: " + ++index + "\n");
-            session.send("Title: " + item.volumeInfo.title);
-            session.send("previewLink: " + item.volumeInfo.previewLink);
-          }
-        });
-      });
-
+      }
     }
-]);
+  }
 
-dialog.onDefault(builder.DialogAction.send("You're asking me too much. I'm a bot!"));
+  res.status(200).send("OK");
+});
+
+
+// G?i thông tin t?i REST API ð? tr? l?i
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "token",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
