4e2416495dfe17e27e2b464fd667f1011b97632a
leductienttkt <leductienttkt@gmail.com>
2016-11-19 19:42:13 +0700

    Update findEbook.js

--- findEbook.js
+++ findEbook.js
@@ -105,7 +105,7 @@ function sendMessage(senderId, where) {
 			
         
 							kq = wt.name +"\n" 
-							+ "Thời tiết hiện tại: " + dateObj.toString() +" m \n"
+							+ "Thời tiết hiện tại: " + dateObj.getDate() +" m \n"
 							+ "Nhiệt độ hiện tại: " + (parseInt(wt.main.temp) -273)  + " độ C \n"
 							+ "Nhiệt độ tối đa: " + (parseInt(wt.main.temp_min) -273)  + " độ C \n"
 							+ "Nhiệt độ tối thiểu: " +(parseInt(wt.main.temp_max) -273)  + " độ C \n"
